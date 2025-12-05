7065f267b896318f56ed9fe24e3e0fa90cc97a16
florizotto <florie.mesple@gmail.com>
2025-12-05 11:07:48 +0100

    removed sys

--- src/l2ccowsay/roar.py
+++ src/l2ccowsay/roar.py
@@ -1,7 +1,4 @@
-import sys
-sys.path.insert(0, r"C:\Users\yassi\l2ccowsay\src\l2ccowsay")
-
-from characters import DRAGON
+from .characters import DRAGON
 
 def roar(txt: str = 'rrrrrr'):
     tt = f'{"="*(len(txt)+4)}\n'
